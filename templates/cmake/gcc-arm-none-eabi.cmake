aea9988bb247111ca2b4e0ac7167068610a71da4
Manny Bains <manjinder.bains@gmail.com>
2023-01-29 07:25:08 -0800

    Issue 16: cflags in linux

--- templates/cmake/gcc-arm-none-eabi.cmake
+++ templates/cmake/gcc-arm-none-eabi.cmake
@@ -7,9 +7,12 @@ set(TOOLCHAIN_PREFIX                arm-none-eabi-)
 set(FLAGS                           "-fdata-sections -ffunction-sections --specs=nano.specs -Wl,--gc-sections")
 set(CPP_FLAGS                       "-fno-rtti -fno-exceptions -fno-threadsafe-statics")
 
-set(CMAKE_C_COMPILER                ${TOOLCHAIN_PREFIX}gcc ${FLAGS})
+set(CMAKE_C_FLAGS                   ${FLAGS})
+set(CMAKE_CPP_FLAGS                 ${FLAGS} ${CPP_FLAGS})
+
+set(CMAKE_C_COMPILER                ${TOOLCHAIN_PREFIX}gcc)
 set(CMAKE_ASM_COMPILER              ${CMAKE_C_COMPILER})
-set(CMAKE_CXX_COMPILER              ${TOOLCHAIN_PREFIX}g++ ${FLAGS} ${CPP_FLAGS})
+set(CMAKE_CXX_COMPILER              ${TOOLCHAIN_PREFIX}g++)
 set(CMAKE_OBJCOPY                   ${TOOLCHAIN_PREFIX}objcopy)
 set(CMAKE_SIZE                      ${TOOLCHAIN_PREFIX}size)
 
